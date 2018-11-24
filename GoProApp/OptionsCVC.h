2f0990e808bf6a1a1f0a196e29157784a1c3505e
JDSL <jdsqrpnts@yahoo.com>
2018-11-24 00:43:43 -0800

    Removed files and created new Options Collection VC

---
+++ GoProApp/OptionsCVC.h
@@ -0,0 +1,13 @@
+//
+//  OptionsCVC.h
+//  GoProApp
+//
+//  Created by JDSL on 11/24/18.
+//  Copyright © 2018 JD Leonard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OptionsCVC : UICollectionViewController
+
+@end
